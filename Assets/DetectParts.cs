8701aa9142951d4d7af9a768788515881c0b8798
Baray-Ter <m.barayter@hotmail.com>
2023-05-05 22:43:16 +0300

    initial commit

--- Assets/DetectParts.cs
+++ Assets/DetectParts.cs
@@ -1,38 +1,77 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 using TMPro;
 
 public class DetectParts : MonoBehaviour
 {
-    public LayerMask ignoredLayerMask;
-
     private TextAsset getTextAsset;
 
-    public TextMeshProUGUI informationBubble;
+    public TextMeshProUGUI informationText;
+
+    Camera mainCamera;
+
+    public GameObject textGameObject;
+
+    private bool setTextGameObjectState;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void FixedUpdate()
     {
-        RaycastHit hitInfo;
+        // Vector3 mousePos = Mouse.current.position.ReadValue();
+        // mousePos.z = 100f;
+        // mousePos = mainCamera.ScreenToWorldPoint(mousePos);
+        // Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
+        // Debug.Log(transform.name);
 
-        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Vector2 _mousePosFromCamera = Mouse.current.position.ReadValue();
+        _mousePosFromCamera = mainCamera.ScreenToWorldPoint(_mousePosFromCamera);
+        // RaycastHit raycastHit = Physics.Raycast(transform.position, _mousePosFromCamera);
+        // Debug.DrawRay(transform.position, _mousePosFromCamera, Color.red);
+        // Debug.Log(transform.name);
 
-        Ray rayOrigin = Camera.main.ScreenPointToRay(mousePosition);
+        // RaycastHit hit = Physics.Raycast(transform.position, _mousePosFromCamera, Mathf.Infinity);
+        // Does the ray intersect any objects excluding the player layer
 
-        if (Physics.Raycast(rayOrigin, out hitInfo))
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, _mousePosFromCamera, out hit, Mathf.Infinity))
         {
-            if (hitInfo.transform.GetComponent<HandleText>().textInformation == null)
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+    }
+
+    public void MouseRightClick(InputAction.CallbackContext context){
+        
+        if (Mouse.current.rightButton.isPressed && context.started)
+        {
+            setTextGameObjectState = !setTextGameObjectState;
+            textGameObject.SetActive(setTextGameObjectState);
+        }
+    }
+
+    private void OnMouseDrag() {
+        
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        mousePos.z = 100f;
+        mousePos = mainCamera.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray,out hit, 1000))
             {
-                return;
+                getTextAsset = hit.transform.GetComponent<HandleText>().textInformation;
+                informationText.text = getTextAsset.text;
             }
-
-            Debug.Log(hitInfo.transform.name);
-
-            getTextAsset = hitInfo.transform.GetComponent<HandleText>().textInformation;
-
-            informationBubble.text = getTextAsset.text;
         }
     }
 }
--- Assets/ModelRotation.cs
+++ Assets/ModelRotation.cs
@@ -7,9 +7,9 @@ public class ModelRotation : MonoBehaviour
 {
     public float rotationSpeed = 10f;
 
-    private void FixedUpdate()
-    {
-        if (Mouse.current.leftButton.IsPressed(0))
+    private void FixedUpdate() {
+        
+        if (Mouse.current.leftButton.isPressed)
         {
             float rotateX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
             float rotateY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
